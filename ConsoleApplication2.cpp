1ed73b02c219514e877eb3bf268191b1d416c039
FairuzDiana <55312572+FairuzDiana@users.noreply.github.com>
2019-11-26 09:44:06 +0800

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <iomanip>
+
+using namespace std;
+
+void main()
+{
+	char name[25];
+	char id[5];
+	float m1, m2, m3, m4, m5;
+
+	ofstream outputFile("studentMarks.text", ios::out);
+
+	if (!outputFile)
+	{
+		cerr << "File could not be opened \n";
+		exit(1);
+	}
+
+	cout << "please enter the student id, name and five marks \n";
+	cout << "\npress <ctrl> + z to stop. \n";
+
+	while (cin >> id >> name >> m1 >> m2 >> m3 >> m4 >> m5)
+	{
+
+		outputFile << id << " " << name << " " << m1 << " " << m2 << " " << m3 << " " << m4 << " " << m5 << endl;
+	}
+}
